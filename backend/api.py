19dad810890cc8473a22922c04063adefa4c0a1c
Guatier.Klein <klein_gautier@yahoo.Fr>
2025-03-08 22:10:25 +0100

    final regression

--- backend/api.py
+++ backend/api.py
@@ -16,8 +16,12 @@ app.add_middleware(
 )
 
 # Charger les coefficients depuis le fichier CSV
-coefficients = pd.read_csv("coefficients_withreviews.csv")
-coef_dict = dict(zip(coefficients["Variable"], coefficients["Coefficient"]))
+coefficients = pd.read_csv("coef_poly_noreview.csv")
+neighbourhood_prices = pd.read_csv("neighbourhood_prices.csv")
+property_prices = pd.read_csv("property_prices.csv")
+coef_dict = dict(zip(coefficients["Variable"], coefficients["Coeff."]))
+neighbourhood_dict = dict(zip(neighbourhood_prices["neighbourhood"], neighbourhood_prices["Mean(price)"]))
+property_dict = dict(zip(property_prices["property_type"], property_prices["Mean(price)"]))
 
 # Définir le modèle de données pour l'input utilisateur
 class PredictionInput(BaseModel):
@@ -27,78 +31,60 @@ class PredictionInput(BaseModel):
     beds: int
     room_type: str
     country: str
+    property_type : str 
+    neighbourhood : str
 
-class PredictionInputWithReview(BaseModel):
-    bathrooms: float
-    accomodate: int
-    bedrooms: int
-    beds: int
-    room_type: str
-    country: str
-    review_scores_rating : float
-    review_scores_accuracy : float
-    review_scores_cleanliness : float
-    review_scores_checkin : float
-    review_scores_communication : float
-    review_scores_location : float
+def get_neighbourhood_mean_price(neighbourhood):
+    return neighbourhood_dict.get(neighbourhood, neighbourhood_dict["Unknown"])
 
-# Fonction pour prédire un prix
-def predict_price(input_data: PredictionInput):
-    price = coef_dict["Intercept"]
+def get_property_type_mean_price(property_type):
+    return property_dict.get(property_type)
+
+def apply_polynomial_model(input_data, coef_dict):
+    """Applique les coefficients d'un modèle de régression polynomiale à un ensemble de données d'entrée."""
+    price = coef_dict.get("Intercept", 0)
     
-    price += coef_dict["bathrooms"] * input_data.bathrooms
-    price += coef_dict["accomodate"] * input_data.accomodate
-    price += coef_dict["bedrooms"] * input_data.bedrooms
-    price += coef_dict["beds"] * input_data.beds
-
-    room_types = ["Shared room", "Hotel room"]
-    for room in room_types:
-        if input_data.room_type == room and room in coef_dict:
-            price += coef_dict[room]
-
-    countries = ["Australie", "Argentine", "Thaïlande", "USA", "Bresil", "France", "Japon", "Espagne"]
-    for country in countries:
-        if input_data.country == country and country in coef_dict:
-            price += coef_dict[country]
-
+    for var, coef in coef_dict.items():
+        if var == "Intercept":
+            continue
+        
+        if "^" in var:  # Gestion des termes quadratiques
+            base_var, exponent = var.split("^")
+            exponent = int(exponent)
+            if base_var in input_data:
+                price += coef * (input_data[base_var] ** exponent)
+        
+        elif "*" in var:  # Gestion des termes d'interaction
+            vars_interaction = var.split("*")
+            if all(v in input_data for v in vars_interaction):
+                product = np.prod([input_data[v] for v in vars_interaction])
+                price += coef * product
+        
+        elif var in input_data:  # Gestion des termes linéaires
+            price += coef * input_data[var]
+    
     return price
 
-# Fonction pour prédire un prix avec review
-def predict_price_review(input_data: PredictionInputWithReview):
-    price = coef_dict["Intercept"]
-    
-    price += coef_dict["bathrooms"] * input_data.bathrooms
-    price += coef_dict["accomodate"] * input_data.accomodate
-    price += coef_dict["bedrooms"] * input_data.bedrooms
-    price += coef_dict["review_scores_rating"] * input_data.review_scores_rating
-    price += coef_dict["review_scores_accuracy"] * input_data.review_scores_accuracy
-    price += coef_dict["review_scores_cleanliness"] * input_data.review_scores_cleanliness
-    price += coef_dict["review_scores_checkin"] * input_data.review_scores_checkin
-    price += coef_dict["review_scores_communication"] * input_data.review_scores_communication
-    price += coef_dict["review_scores_location"] * input_data.review_scores_location
-
-    room_types = ["Shared room", "Hotel room"]
-    for room in room_types:
-        if input_data.room_type == room and room in coef_dict:
-            price += coef_dict[room]
-
-    countries = ["Australie", "Argentine", "Thaïlande", "USA", "Bresil", "France", "Japon", "Espagne"]
-    for country in countries:
-        if input_data.country == country and country in coef_dict:
-            price += coef_dict[country]
-
-    return price
+def one_hot_encode(country, cat_list):
+    """Encode en one-hot encoding en fonction de la liste donnée."""
+    encoding = {c: 1 if c == country else 0 for c in cat_list}
+    return encoding
 
 # Définir l'endpoint POST pour prédire le prix
 @app.post("/predict")
 async def predict(input_data: PredictionInput):
     print("input data : ", input_data)
-    predicted_price = predict_price(input_data)
+    
+    country_list = ["Australie", "Argentine", "Thaïlande", "USA", "Bresil", "France", "Japon", "Espagne"]
+    room_types = ["Shared room", "Hotel room"]
+    input_dict = input_data.dict()
+    input_dict.update(one_hot_encode(input_data.country, country_list))
+    input_dict.update(one_hot_encode(input_data.room_type, room_types))
+    
+    input_dict["neighbourhood"] = get_neighbourhood_mean_price(input_data.neighbourhood)
+    input_dict["property_type"] = get_property_type_mean_price(input_data.property_type)
+    
+    predicted_price = apply_polynomial_model(input_dict, coef_dict)
     return {"predicted_price": round(predicted_price, 2)}
 
-# Définir l'endpoint POST pour prédire le prix
-@app.post("/predict_with_reviews")
-async def predict_review(input_data: PredictionInputWithReview):
-    print("input data : ", input_data)
-    predicted_price = predict_price_review(input_data)
-    return {"predicted_price": round(predicted_price, 2)}
+
--- frontend/script.js
+++ frontend/script.js
@@ -1,4 +1,4 @@
-async function sendPredictionRequest(withreviews) {
+async function sendPredictionRequest() {
     url = "predict"
 
     // Get input values
@@ -6,16 +6,10 @@ async function sendPredictionRequest(withreviews) {
     let accomodate = parseInt(document.getElementById("accomodate").value);
     let bedrooms = parseInt(document.getElementById("bedrooms").value);
     let beds = parseInt(document.getElementById("beds").value);
-
     let room_type = document.getElementById("room_type").value;
     let country = document.getElementById("country").value;
-
-    let review_scores_rating 
-    let review_scores_accuracy 
-    let review_scores_cleanliness 
-    let review_scores_checkin 
-    let review_scores_communication
-    let review_scores_location 
+    let property_type = document.getElementById("property_type").value;
+    let neighbourhood = document.getElementById("neighbourhood").value;
 
     let data = {
         "bathrooms": bathrooms,
@@ -23,27 +17,10 @@ async function sendPredictionRequest(withreviews) {
         "bedrooms": bedrooms,
         "beds": beds,
         "room_type": room_type,
-        "country": country
+        "country": country,
+        "property_type": property_type,
+        "neighbourhood": neighbourhood
     };
-
-    if(withreviews) {
-        review_scores_rating = parseFloat(document.getElementById("review_scores_rating").value);
-        review_scores_accuracy = parseFloat(document.getElementById("review_scores_accuracy").value);
-        review_scores_cleanliness = parseFloat(document.getElementById("review_scores_cleanliness").value);
-        review_scores_checkin = parseFloat(document.getElementById("review_scores_checkin").value);
-        review_scores_communication = parseFloat(document.getElementById("review_scores_communication").value);
-        review_scores_location = parseFloat(document.getElementById("review_scores_location").value);
-
-        data["review_scores_rating"] = review_scores_rating
-        data["review_scores_accuracy"] = review_scores_accuracy
-        data["review_scores_cleanliness"] = review_scores_cleanliness
-        data["review_scores_checkin"] = review_scores_checkin
-        data["review_scores_communication"] = review_scores_communication
-        data["review_scores_location"] = review_scores_location
-
-        url = "predict_with_reviews"
-    }
-
 
     try {
         let response = await fetch(`http://127.0.0.1:8000/${url}`, {
